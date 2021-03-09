043025fc2814dfdb0af00138154f7bdcf68757c9
Hoàng Tuấn <Tuanit2304@gmail.com>
2021-03-09 17:30:49 +0700

    start js

--- js/page/employee.js
+++ js/page/employee.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
-  
+  loadData();
 });
 
 function loadData() {
-    alert(arguments[1]);
-  }  
+//get Data
+  $.ajax({
+    url:"http://api.manhnv.net/api/employees",
+    method: "GET",
+  }).done(function(res){
+    var data = res;
+    debugger; 
+  }).fail(function (res) {
+    
+  })
+//binding data
+}  
