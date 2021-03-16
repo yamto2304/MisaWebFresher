af5b637064050faef50f2bdab49d60f798bb4749
Hoàng Tuấn <Tuanit2304@gmail.com>
2021-03-16 17:58:23 +0700

    cannot click tag <tr> in table

--- js/page/list.js
+++ js/page/list.js
@@ -1,24 +1,27 @@
 $(document).ready(function(){
     loadData();
-    // $('#add-form').close();
     // var tableJs = document.getElementById('tbl-customer');
     // var tableJq = $('#tbl-customer');
     var btnOpenFormAdd = $('#buttonAdd');
     var btnCloseForm = $('#btnCancel');
     var btnSave = $('#btnSave');
-    // debugger
     btnOpenFormAdd.click(function(){
-        //  $(this).hide(); 
-        // debugger
-        $('#add-form').show();
+        document.getElementById('add-form').showModal();
     })
-
+    //Add customer
     btnSave.click(function(){
         saveData();
     })
-    //event 
+    //Close form
     btnCloseForm.click(function(){
         $('#add-form').hide();
+    })
+    var rowData = $("#body-data tr");
+    // debugger
+    rowData.click(function() {
+        debugger
+        // showRowOptions();
+        $(this).hide(); 
     })
 })
 // get form data
@@ -35,28 +38,49 @@ function getCustomerInForm(){
     return customer;
 }
 
+function setCustomerInform(params) {
+    
+}
+
+function showRowOptions() {
+    $('#add-form').showModal();
+}
+
 function loadData(){
+    
     $.ajax({
         url: "http://api.manhnv.net/api/customers",
         method: "GET"
     }).done(function(response){
         $('#tbl-customer tbody').empty();
         for(var i = 0; 0 <response.length; i++){
-            var trHtml = $(`<tr class="firstline">
+            var customerId = response[i].CustomerId;
+            // debugger
+            var trHtml = $(`<tr class="datarow" id=${response[i].CustomerId} >
                 <td>${response[i].CustomerCode}</td>
                 <td>${response[i].FullName}</td>
                 <td>${response[i].Genger == null ? "" : formatGender(response[i].Gender)}</td>
+                <td>${response[i].Address}</td>
                 <td>${response[i].DateOfBirth /* == null ? "" : formartDate(response[i].DateOfBirth)*/}</td>
-                <td>${response[i].PhoneNumber}</td>
                 <td>${response[i].Email == null ? "" : response[i].Email}</td>
+                <td>${response[i].PhoneNumber}</td>
                 <td>${response[i].CustomerGroupName}</td>
+                <td>${response[i].CompanyName}</td>
+                <td>${response[i].IsStopFollow == true ? "Đang theo dõi" : "Không"}</td>
+                <td><button id="edit>Sửa</button><td>
             </tr>`);
             trHtml.data("id", response[i].CustomerId);
+            // debugger
             $('#tbl-customer > tbody:last-child').append(trHtml);
+            //Set style for datarow
+            // $(customerId).css("color","red");
+            document.getElementById(customerId).style.color = "red";
+            //  debugger
         }
     }).fail(function(res){
         // request fail
     });
+    
 }
 
 //format giới tính
@@ -82,7 +106,7 @@ function saveData(){
             //thong bao nguoi dung
             alert("Thêm mới khách hàng thành công");
             // dong dialog
-            document.getElementById('#add-form').closest();
+            $('#add-form').hide();
             //reload data
             loadData();
         }).fail(function(res){
@@ -91,12 +115,12 @@ function saveData(){
     }
 }
 
-// function validate(customer){
-//     if(customer.CustomerCode == null) || customer.CustomerCode == ''){
-//         alert('Mã khách hàng không được bỏ trống');
-//         $('#customerCode')
-//         return false;
-//     }
-//     //....
-//     return true;
-// }
+function validate(customer){
+    if(customer.CustomerCode == null || customer.CustomerCode == ''){
+        alert('Mã khách hàng không được bỏ trống');
+        $('#customerCode')
+        return false;
+    }
+    //....
+    return true;
+}
