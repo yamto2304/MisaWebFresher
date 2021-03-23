df11305af8eee654fac3b70b2e5c74cc0b7a2f55
Hoàng Tuấn <Tuanit2304@gmail.com>
2021-03-23 13:48:20 +0700

    before install vuetify

--- second-app/src/App.vue
+++ second-app/src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <v-main> -->
+      <Navbar/> 
+      <Header/>
+    <!-- </v-main> -->
+    <!-- <Content/>  -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Content from './components/layout/Content.vue'
+import Header from './components/layout/Header.vue'
+import Navbar from './components/layout/Navbar.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    // Content,
+    Header
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='css'>
+  @import url('../style/general.css');
 </style>
---
+++ second-app/src/components/layout/Content.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg = "This is content" }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+  data() {
+    return {
+      author : "Mel"
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+<style scoped>
+
+</style>
