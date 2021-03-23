8bd9f05b455e272b7d0aad74e461c2b75b91145a
Hoàng Tuấn <Tuanit2304@gmail.com>
2021-03-23 17:39:55 +0700

    let start route !

--- second-app/src/App.vue
+++ second-app/src/App.vue
@@ -3,23 +3,22 @@
     <!-- <v-main> -->
       <Navbar/> 
       <Header/>
+      <Content/> 
     <!-- </v-main> -->
-    <!-- <Content/>  -->
   </div>
 </template>
 
 <script>
-// import Content from './components/layout/Content.vue'
-import Header from './components/layout/Header.vue'
+import Content from './components/layout/Content.vue'
 import Navbar from './components/layout/Navbar.vue'
-
-
+import Header from './components/layout/Header.vue'
+// import Content from './components/view/EmployeeList.vue'
 export default {
   name: 'App',
   components: {
     Navbar,
-    // Content,
-    Header
+    Header,
+    Content,
   }
 }
 </script>
