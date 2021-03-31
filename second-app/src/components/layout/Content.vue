078ffd06a0a7d0dced1f4554301707a026493579
TUAN HOANG DINH <hdtuan@rd.local>
2021-03-31 10:23:27 +0700

    test6

---
+++ second-app/src/components/layout/Content.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="content">
+    <!-- <h1>This is the main page</h1> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+  data() {
+    return {
+      author : "Mel"
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ second-app/src/components/view/CustomerList.vue
@@ -0,0 +1,17 @@
+<template>
+   <div>This is customer page</div>
+</template>
+
+<script>
+export default {
+  name: 'Customer',
+  data() {
+    return {
+      author : "Mel"
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
