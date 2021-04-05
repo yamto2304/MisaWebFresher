f8e93fbd41adc1ed4a352882ce35633d4db9d87e
TUAN HOANG DINH <hdtuan@rd.local>
2021-04-05 18:03:51 +0700

    complete check onblur, need to show alert

--- second-app/src/components/form/AddForm.vue
+++ second-app/src/components/form/AddForm.vue
@@ -1,8 +1,11 @@
 <style scoped>
-.isHide{
+.isHide {
   display: none;
 }
-.red-text{
+.isBlurAlert {
+  border-color: red;
+}
+.red-text {
   color: red;
 }
 .dialog-modal {
@@ -13,13 +16,13 @@
   right: 0;
   background-color: black;
   opacity: 0.4;
-    z-index: 999;
+  z-index: 999;
 }
 .m-dialog {
   z-index: 998;
 }
 .dialog-content {
-  position:fixed;
+  position: fixed;
   width: 700px;
   height: 583px;
   left: calc(50% - 400px);
@@ -27,9 +30,9 @@
   border-radius: 5px;
   background-color: #fff;
   border: 1px solid #bbbbbb;
-    z-index: 1000;
+  z-index: 1000;
 }
-.btn-close-form{
+.btn-close-form {
   position: absolute;
   top: 16px;
   right: 16px;
@@ -52,129 +55,120 @@
   display: flex;
   font-size: 24px;
 }
-/* =========================== */
-/* .form-add-customer {
-    width: 700px;
-    height: 564px;
-    border-radius: 4px;
-    outline: none;
-    border: 1px solid #bbbbbb;
-} */
 
 .basic-info {
-    width: 100%;
-    height: 228px;
-    display: flex;
+  width: 100%;
+  height: 228px;
+  display: flex;
 }
-  .insert-image {
-      width: 200px;
-      height: 100%;
-      /* margin-right: 16px; */
-      margin-left: 24px;
-      /* border: 1px solid yellow; */
-  }
+.insert-image {
+  width: 200px;
+  height: 100%;
+  /* margin-right: 16px; */
+  margin-left: 24px;
+  /* border: 1px solid yellow; */
+}
 
-  .insert-image .avatar {
-      border: 1px solid #bbbbbb;
-      border-radius: 50%;
-      width: 100%;
-      height: 188px;
-      background-image: url('../../assets/img/default-avatar.jpg');
-      background-position: center;
-      background-repeat: no-repeat;
-      background-size: contain;
-  }
-  .insert-image .hint{
-      text-align: center;
-      width: 100%;
-  }
+.insert-image .avatar {
+  border: 1px solid #bbbbbb;
+  border-radius: 50%;
+  width: 100%;
+  height: 188px;
+  background-image: url("../../assets/img/default-avatar.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+.insert-image .hint {
+  text-align: center;
+  width: 100%;
+}
 
-  .basic-info .col-left-1{
-      width: 200px;
-      height: 100%;
-      margin-left: 24px;
-      margin-right: 16px;
-  }
+.basic-info .col-left-1 {
+  width: 200px;
+  height: 100%;
+  margin-left: 24px;
+  margin-right: 16px;
+}
 
-      .input-customer-field{
-          margin-top: 12px;
-          
-      }
+.input-customer-field {
+  margin-top: 12px;
+}
 
-          .input-box-small{
-            width: 200px;
-              margin-top: 4px;
-          }
+.input-box-small {
+  width: 200px;
+  margin-top: 4px;
+}
 
-          .input-date{
-              width: 201px;
-              height: 40px;
-          }
+.input-date {
+  width: 201px;
+  height: 40px;
+}
 
-          .input-box-medium{
-              width: 424px;
-              margin-top: 4px;   
-          }
+.input-box-medium {
+  width: 424px;
+  margin-top: 4px;
+}
 
-          .input-box-large{
-              margin-top: 4px;
-              width: 640px;
-          }
-      
-      .radio-box-small{
-          margin-top: 4px;
-          align-items: center;
-          text-align: left;
-          height: 36px;
-          line-height: 36px;
-      }
+.input-box-large {
+  margin-top: 4px;
+  width: 640px;
+}
 
-  .col-right{
-      width: 200px;
-      height: 100%;
-      margin-right: 24px;
-  }
-      .group-customer{
-          border: none;
-          margin-top: 4px;
-          width: 202px;
-          height: 36px;
-          border-radius: 4px;
-          border: 1px solid #bbbbbb;
-          color: #000000;
-      }
+.radio-box-small {
+  margin-top: 4px;
+  align-items: center;
+  text-align: left;
+  height: 36px;
+  line-height: 36px;
+}
 
-  .contact-info .col-left-2{
-      width: 424px;
-      height: 100%;
-      margin-left: 24px;
-      margin-right: 16px;
-  }
+.col-right {
+  width: 200px;
+  height: 100%;
+  margin-right: 24px;
+}
+.group-customer {
+  border: none;
+  margin-top: 4px;
+  width: 202px;
+  height: 36px;
+  border-radius: 4px;
+  border: 1px solid #bbbbbb;
+  color: #000000;
+}
 
-.contact-info{
-    display: flex;
-    width: 100%;
-    height: 242px;
+.contact-info .col-left-2 {
+  width: 424px;
+  height: 100%;
+  margin-left: 24px;
+  margin-right: 16px;
+}
+
+.contact-info {
+  display: flex;
+  width: 100%;
+  height: 242px;
 }
 
 .form-footer {
-    display: flex;
-    width: 100%;
-    height: 60px;
-    background-color: #BBBBBB;
-    margin-top: 0px;
-    outline: none;
-    line-height: 60px;
-    /* align-self: center;
+  display: flex;
+  width: 100%;
+  height: 60px;
+  background-color: #bbbbbb;
+  margin-top: 0px;
+  outline: none;
+  line-height: 60px;
+  /* align-self: center;
     align-content: center; */
-    border-bottom-right-radius: 5px;
-    border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  border-bottom-left-radius: 5px;
 }
-.btn-cancel{
+.btn-cancel {
   position: absolute;
   left: 424px;
   align-self: center;
-  background-color: #BBBBBB;
+  background-color: #bbbbbb;
   height: 40px;
   width: 100px;
   border-radius: 4px;
@@ -182,7 +176,7 @@
   line-height: 60px;
   /* background-color: red; */
 }
-.btn-another{
+.btn-another {
   display: flex;
   background-color: #019160;
   align-items: center;
@@ -193,17 +187,17 @@
   text-align: center;
   border: none;
   padding-left: 36px;
-  background-color: #BBBBBB;
+  background-color: #bbbbbb;
   cursor: pointer;
 }
-  .btn-another:active{
-    background-color: #019160;
-    border: none;
-  }
-  .btn-another:hover{
-    background-color: aliceblue;
-  }
-.btn-save{
+.btn-another:active {
+  background-color: #019160;
+  border: none;
+}
+.btn-another:hover {
+  background-color: aliceblue;
+}
+.btn-save {
   position: absolute;
   left: 524px;
   margin-left: 40px;
@@ -211,124 +205,231 @@
 }
 </style>
 <template>
-    <div class="m-dialog"
+  <div
+    class="m-dialog"
     title="Thông tin khách hàng"
-    :class="{ isHide:isHide }"
-    >
-      <div class="dialog-modal"></div>
-      <div class="dialog-content">
-        <div class="form-header">
-            <div class="heading">
-                <b>Thông tin khách hàng</b>
-            </div>
-            <div class="btn-close-form">
-              <button v-on:click="btnCancelOnClick">x</button>
-            </div>
+    :class="{ isHide: isHide }"
+  >
+    <div class="dialog-modal"></div>
+    <div class="dialog-content">
+      <div class="form-header">
+        <div class="heading">
+          <b>Thông tin khách hàng</b>
         </div>
-        <div class="basic-info">
-            <div class="insert-image">
-                <div class="avatar"></div>
-                <div class="hint">Vui lòng chọn ảnh có định dạng <b>.jpg, .jpeg, .png, .gif.</b></div>
-            </div>
-            <div class="col-left-1">
-                <div class="input-customer-field">
-                    <label >Mã khách hàng (<span class="red-text">*</span>)</label>
-                    <input type="text" name ="customer-code" id="customerCode" onblur="validate(getCustomerInForm())" class="input-box-small" required>
-                </div>  
-                <div class="input-customer-field">
-                    <label >Mã thẻ thành viên</label>
-                    <input name="member-code" id="memberCode" class="input-box-small">
-                </div>
-                <div class="input-customer-field">
-                    <label >Ngày sinh</label>
-                    <input type="date" name="date-of-birth" id="dateOfBirth" class="input-box-small input-date">
-                </div>  
-            </div>
-            <div class="col-right">
-                <div class="input-customer-field">
-                    <label>Họ và tên (<span class="red-text">*</span>)</label>
-                    <input type="text" class="input-box-small" id="fullName" required>
-                </div> 
-                <div class="input-customer-field">
-                    <label>Nhóm khách hàng</label>
-                    <select class="group-customer" id=".customerGroupName">
-                        <option value="1">Nhóm1</option>
-                        <option value="2">Nhóm2</option>
-                    </select>
-                </div> 
-                <div class="input-customer-field">
-                    <label>Giới tính</label>
-                    <div class="radio-box-small" id="gender">
-                        <label><input  type="radio" checked name="gender" value="1"><span>Nam</span></label>
-                        <label><input  type="radio"  name="gender" value="2"><span>Nữ</span></label>
-                        <label><input  type="radio"  name="gender" value="3"><span>Khác</span></label>
-                    </div>
-                </div> 
-            </div>
-
+        <div class="btn-close-form">
+          <button v-on:click="btnCancelOnClick">x</button>
         </div>
-        <div class="contact-info">
-            <div class="col-left-2">
-                <div class="input-customer-field">
-                    <label>Email</label>
-                    <input type="email" id="email" placeholder="example@domain.com" class="input-box-medium" >
-                </div>
-                <div class="input-customer-field">
-                    <label>Nhóm khách hàng</label>
-                    <input type="text" id="customerGroupName" class="input-box-medium" >
-                </div>
-                <div class="input-customer-field">
-                    <label>Địa chỉ</label>
-                    <input type="text" id="address" class="input-box-large">
-                </div>
-            </div>
-            <!-- SDT -->
-            <div class="col-right">
-                <div class="input-customer-field">
-                    <label>Số điện thoại (<span class="red-text">*</span>)</label>
-                    <input type="text" class="input-box-small" id="phoneNumber" required>
-                </div> 
-                <!-- Ms thue -->
-                <div class="input-customer-field">
-                    <label>Tên công ty</label>
-                    <input type="text" placeholder="Tên công ty" id="companyName" class="input-box-small">
-                </div>   
-            </div>
-
+      </div>
+      <div class="basic-info">
+        <div class="insert-image">
+          <div class="avatar"></div>
+          <div class="hint">
+            Vui lòng chọn ảnh có định dạng <b>.jpg, .jpeg, .png, .gif.</b>
+          </div>
         </div>
-        <div class="form-footer">
-            <div class="btn-cancel" style="vertical-align: top;">
-                <button class="btn-another"
-                v-on:click="btnCancelOnClick"
-                >Hủy</button>
-            </div>
-            <div class="btn-save"  id="btnSave" >
-                <button class="btn-with-icon" 
-                style="width:100px"
-                >
-                    <div class="icon-btn icon-save"></div>
-                    <div class="text-btn">Lưu</div>
-                </button>
-            </div>
+        <div class="col-left-1">
+          <div class="input-customer-field">
+            <label>Mã khách hàng(<span class="red-text">*</span>)</label>
+            <input
+              type="text"
+              name="customer-code"
+              id="customerCode"
+              v-on:blur="blurCustomerCode()"
+              v-model="customer.CustomerCode"
+              class="input-box-small"
+              :class="{ isBlurAlert: customerCodeAlert }"
+              required
+            />
+          </div>
+          <div class="input-customer-field">
+            <label>Mã thẻ thành viên</label>
+            <input name="member-code" id="memberCode" class="input-box-small" />
+          </div>
+          <div class="input-customer-field">
+            <label>Ngày sinh</label>
+            <input
+              type="date"
+              name="date-of-birth"
+              id="dateOfBirth"
+              class="input-box-small input-date"
+            />
+          </div>
         </div>
-      </div> 
+        <div class="col-right">
+          <div class="input-customer-field">
+            <label>Họ và tên (<span class="red-text">*</span>)</label>
+            <input
+              type="text"
+              class="input-box-small"
+              id="fullName"
+              required
+              v-model="customer.FullName"
+              :class="{ isBlurAlert: fullNameAlert }"
+              v-on:blur="blurName()"
+            />
+          </div>
+          <div class="input-customer-field">
+            <label>Nhóm khách hàng</label>
+            <select class="group-customer" id=".customerGroupName">
+              <option value="1">Nhóm1</option>
+              <option value="2">Nhóm2</option>
+            </select>
+          </div>
+          <div class="input-customer-field">
+            <label>Giới tính</label>
+            <div class="radio-box-small" id="gender">
+              <label
+                ><input type="radio" checked name="gender" value="1" /><span
+                  >Nam</span
+                ></label
+              >
+              <label
+                ><input type="radio" name="gender" value="2" /><span
+                  >Nữ</span
+                ></label
+              >
+              <label
+                ><input type="radio" name="gender" value="3" /><span
+                  >Khác</span
+                ></label
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="contact-info">
+        <div class="col-left-2">
+          <div class="input-customer-field">
+            <label>Email {{customer.Email}}</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="example@domain.com"
+              class="input-box-medium"
+              v-model="customer.Email"
+              v-on:blur="blurEmail()"
+            />
+          </div>
+          <div class="input-customer-field">
+            <label>Nhóm khách hàng</label>
+            <input
+              type="text"
+              id="customerGroupName"
+              class="input-box-medium"
+            />
+          </div>
+          <div class="input-customer-field">
+            <label>Địa chỉ</label>
+            <input type="text" id="address" class="input-box-large" />
+          </div>
+        </div>
+        <!-- SDT -->
+        <div class="col-right">
+          <div class="input-customer-field">
+            <label>Số điện thoại {{customer.PhoneNumber}}(<span class="red-text">*</span>)</label>
+            <input
+              type="text"
+              class="input-box-small"
+              id="phoneNumber"
+              v-model="customer.PhoneNumber"
+              v-on:blur="blurPhoneNumber()"
+              :class="{ isBlurAlert: phoneNumberAlert }"
+              required
+            />
+          </div>
+          <!-- Ms thue -->
+          <div class="input-customer-field">
+            <label>Tên công ty</label>
+            <input
+              type="text"
+              placeholder="Tên công ty"
+              id="companyName"
+              class="input-box-small"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="form-footer">
+        <div class="btn-cancel" style="vertical-align: top">
+          <button class="btn-another" v-on:click="btnCancelOnClick">Hủy</button>
+        </div>
+        <div class="btn-save" id="btnSave">
+          <button class="btn-with-icon" style="width: 100px">
+            <div class="icon-btn icon-save"></div>
+            <div class="text-btn">Lưu</div>
+          </button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-  props: ['isHide'],   //Giá trị từ parent truyển đến
+  props: {
+    phoneNumberAlert: Boolean,
+    customerCodeAlert : Boolean,
+    fullNameAlert : Boolean,
+    isHide: Boolean,
+  },
+  //Giá trị từ parent truyển đến
   methods: {
-    btnAddOnClick() {
-      // this.isHide = false;
+    btnSaveOnClick() {
+      alert(this.customer.PhoneNumber);
     },
     btnCancelOnClick() {
-      this.$emit('closeForm',true)
-      // this.isHide = true;
-    }
+      this.$emit("closeForm", true);
+    },
+    //OnBlur CustomerCode Input
+    blurCustomerCode() {
+      if (!this.customer.CustomerCode) {
+        alert(this.errorCustomerCodeEmpty);
+        this.customerCodeAlert = true;
+      } else this.customerCodeAlert = false;
+    },
+    //OnBlur FullName Input 
+    blurName() {
+      if (!this.customer.FullName) {
+        alert(this.errorNameEmpty);
+        this.fullNameAlert = true;
+      } else this.fullNameAlert = false;
+    },
+    //OnBlur PhoneNumber Input
+    blurPhoneNumber() {
+      if (!this.customer.PhoneNumber) {
+        //Lỗi để trống
+        alert(this.errorPhoneNumberEmpty);
+        this.phoneNumberAlert = true;
+      } else
+      this.phoneNumberAlert = false;
+    },
+    blurEmail(){
+      if(!this.customer.Email)
+        return
+      if(!this.validateEmail(this.customer.Email)){
+        alert(this.errorEmailFormat);
+      }
+    },
     
+    validateEmail(email) {
+      const re = /\S+@\S+\.\S+/;
+      return re.test(String(email).toLowerCase());
+    },
+    // validateEmail(email) {
+    //   var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //   return re.test(email);
+    // },
+    validatePhoneNumber(input){
+      return (!isNaN(input))&&(parseInt('input', 10) >= 0);
+    }
   },
   data() {
     return {
+      errorEmailFormat: "Định dạng email không đúng",
+      errorEmailEmpty: "Email không được để trống",
+      errorNameEmpty: "Tên khách hàng không được để trống",
+      errorPhoneNumberEmpty: "Số điện thoại không được để trống ",
+      errorPhoneNumberFormat: "Sai định dạng số điện thoại",
+      errorCustomerCodeEmpty: "Mã khách hàng không được để trống",
       employee: {
         DateOfBirth: "1992-06-17T00:00:00",
         Address: "319 Prospect Hill Blvd, Keith Bldg, Boise, Idaho, 76994",
@@ -356,7 +457,21 @@ export default {
         Salary: 16275117,
         WorkStatus: 2,
         WorkStatusName: "Đang thử việc",
-      }
+      },
+      customer: {
+        CustomerCode: "",
+        FullName: "",
+        CustomerCardCode: "",
+        GroupCustomerId: "",
+        DateOfBirth: "",
+        Gender: "",
+        GenderName: "",
+        Email: "",
+        PhoneNumber: "",
+        GroupCustomerName: "",
+        CompanyName: "",
+        Address: "",
+      },
     };
   },
 };
--- second-app/src/components/view/CustomerList.vue
+++ second-app/src/components/view/CustomerList.vue
@@ -1,154 +1,169 @@
 <template>
-<div>
-    <AddForm :isHide="isHideParent" @closeForm="closeForm"/>
- <div class="content-body">
-    <div class="header-content">
-      <div class="title" >Danh sách khách hàng</div>
-      <div class="content-feature">
-          <button 
-          class="btn-with-icon" 
-          id="buttonAddCustomer"
-          v-on:click="btnAddOnClick"
+  <div>
+    <AddForm :isHide="isHideParent" @closeForm="closeForm" />
+    <div class="content-body">
+      <div class="header-content">
+        <div class="title">Danh sách khách hàng</div>
+        <div class="content-feature">
+          <button
+            class="btn-with-icon"
+            id="buttonAddCustomer"
+            v-on:click="btnAddOnClick"
           >
-              <div class="icon-add"></div>
-              <div class="text-btn">Thêm khách hàng</div>
+            <div class="icon-add"></div>
+            <div class="text-btn">Thêm khách hàng</div>
           </button>
+        </div>
       </div>
-    </div>
-    <div class="filter-bar">
-      <div class="filter-left">
-        <input class="icon-search input-search" 
-        type="text" 
-        placeholder="Tìm kiếm theo mã, Tên hoặc Số điện thoại">
-        <select
-          id="cbxDepartment"
-          fieldName="CustomerGroupName"
-          fieldValue="CustomerGroupId"
-          api="/api/customergroups"
-          class="m-control"
-        >
-          <option >
-            Tất cả phòng ban
-          </option>
-          <option >
-            Văn phòng Tổng công ty
-          </option>
-          <option >
-            Phong đào tạo công nghệ
-          </option>
-          <option>
-            Phòng Nhân sự
-          </option>
-        </select>
-        <select
-          id="cbxPosition"
-          fieldName="CustomerGroupName"
-          fieldValue="CustomerGroupId"
-          api="/api/customergroups"
-          class="m-control"
-        >
-          <option>
-            Tất cả vị trí
-          </option>
-          <option>Giám đốc</option>
-          <option>
-            Trưởng phòng
-          </option>
-          <option>
-            Nhân viên
-          </option>
-        </select>
+      <div class="filter-bar">
+        <div class="filter-left">
+          <input
+            class="icon-search input-search"
+            type="text"
+            placeholder="Tìm kiếm theo mã, Tên hoặc Số điện thoại"
+          />
+          <select
+            id="cbxDepartment"
+            fieldName="CustomerGroupName"
+            fieldValue="CustomerGroupId"
+            api="/api/customergroups"
+            class="m-control"
+          >
+            <option>Tất cả phòng ban</option>
+            <option>Văn phòng Tổng công ty</option>
+            <option>Phong đào tạo công nghệ</option>
+            <option>Phòng Nhân sự</option>
+          </select>
+          <select
+            id="cbxPosition"
+            fieldName="CustomerGroupName"
+            fieldValue="CustomerGroupId"
+            api="/api/customergroups"
+            class="m-control"
+          >
+            <option>Tất cả vị trí</option>
+            <option>Giám đốc</option>
+            <option>Trưởng phòng</option>
+            <option>Nhân viên</option>
+          </select>
+        </div>
+        <div class="filter-right">
+          <button class="btn-only-icon btn-refresh" id="refresh">
+            <div class="icon-refresh"></div>
+          </button>
+        </div>
       </div>
-      <div class="filter-right">
-        <button class="btn-only-icon btn-refresh" id="refresh">
-          <div class="icon-refresh"></div>
-        </button>
-      </div>
-    </div>
-    <div class="data-table">
+      <div class="data-table">
         <table id="tbl-customer">
-            <thead>
-                <tr>
-                    <th>Mã khách hàng</th>
-                    <th>Tên khách hàng</th>
-                    <th>Giới tính</th>
-                    <th>Địa chỉ</th>
-                    <th>Ngày sinh</th>
-                    <th>Email</th>
-                    <th>SĐT</th>
-                    <th>Nhóm khách hàng</th>
-                    <th>Tên công ty</th>
-                    <th>Tình trạng theo dõi</th>
-                </tr>
-            </thead>
-            <tbody id="body-data">
-                
-            </tbody>
+          <thead>
+            <tr>
+              <th>Mã khách hàng</th>
+              <th>Tên khách hàng</th>
+              <th>Giới tính</th>
+              <th>Địa chỉ</th>
+              <th>Ngày sinh</th>
+              <th>Email</th>
+              <th>SĐT</th>
+              <th>Nhóm khách hàng</th>
+              <th>Tên công ty</th>
+              <th>Tình trạng theo dõi</th>
+            </tr>
+          </thead>
+          <tbody id="body-data"></tbody>
         </table>
-    </div>
-    <div class="paging-bar">
+      </div>
+      <div class="paging-bar">
         <div class="paging-record-infor">
-            Hiển thị <b>1-10/1000</b> nhân viên
+          Hiển thị <b>1-10/1000</b> nhân viên
         </div>
         <div class="paging-option">
-            <div class="paging-back">
-                <button class="paging-back-all btn-circle"></button>
-                <button class="paging-back-one btn-circle"></button>
-            </div>
-            <div class="paging-number">
-                <label><input class="btn-circle" type="radio" id="bcp" name="page" value="1" checked><span>1</span></label>
-                <label><input class="btn-circle" type="radio" id="bcp" name="page" value="2"><span>2</span></label>
-                <label><input class="btn-circle" type="radio" id="bcp" name="page" value="3"><span>3</span></label>
-                <label><input class="btn-circle" type="radio" id="bcp" name="page" value="4"><span>4</span></label>
-            </div>
-            <div class="paging-next">
-                <button class="paging-next-one btn-circle"></button>
-                <button class="paging-next-all btn-circle"></button>
-            </div>
+          <div class="paging-back">
+            <button class="paging-back-all btn-circle"></button>
+            <button class="paging-back-one btn-circle"></button>
+          </div>
+          <div class="paging-number">
+            <label
+              ><input
+                class="btn-circle"
+                type="radio"
+                id="bcp"
+                name="page"
+                value="1"
+                checked
+              /><span>1</span></label
+            >
+            <label
+              ><input
+                class="btn-circle"
+                type="radio"
+                id="bcp"
+                name="page"
+                value="2"
+              /><span>2</span></label
+            >
+            <label
+              ><input
+                class="btn-circle"
+                type="radio"
+                id="bcp"
+                name="page"
+                value="3"
+              /><span>3</span></label
+            >
+            <label
+              ><input
+                class="btn-circle"
+                type="radio"
+                id="bcp"
+                name="page"
+                value="4"
+              /><span>4</span></label
+            >
+          </div>
+          <div class="paging-next">
+            <button class="paging-next-one btn-circle"></button>
+            <button class="paging-next-all btn-circle"></button>
+          </div>
         </div>
-        <div class="pagin-record-option">
-            <b>10</b> nhân viên/trang
-        </div>
+        <div class="pagin-record-option"><b>10</b> nhân viên/trang</div>
+      </div>
     </div>
-    <!-- <AddForm @closePopup="false" :isHide="false"/> -->
   </div>
-</div>
- 
 </template>
 <script>
-import AddForm from '../form/AddForm'
+import AddForm from "../form/AddForm";
 export default {
-  name: 'Customer',
+  name: "Customer",
   // props: ['isHideParent'],
-  components :{
-    AddForm
+  components: {
+    AddForm,
   },
   methods: {
     btnAddOnClick() {
       this.isHideParent = false;
-      alert("btnAddCustomerOnClick");
+      // alert("btnAddCustomerOnClick");
     },
     //Refresh
-    refresh(){
-        // loadData();
-        window.location.reload();
+    refresh() {
+      // loadData();
+      window.location.reload();
     },
-    //ClosePopup
-    closeForm(value){
+    //CloseForm
+    closeForm(value) {
       this.isHideParent = value;
-    }
+    },
   },
   data() {
     return {
-      author : "Mel",
-      isHideParent : true
-    }
+      author: "Mel",
+      isHideParent: true,
+    };
   },
-}
+};
 </script>
 <style scoped>
 .input-search {
-    padding-left: 40px;
-    width: 300px;
+  padding-left: 40px;
+  width: 300px;
 }
 </style>
