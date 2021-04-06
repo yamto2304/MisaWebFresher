5d6207f81b8305119483cc758faf1c9dd68dd99c
TUAN HOANG DINH <hdtuan@rd.local>
2021-04-06 11:56:52 +0700

    complete item navbar on active

--- second-app/src/App.vue
+++ second-app/src/App.vue
@@ -21,4 +21,10 @@ export default {
 
 <style lang='css'>
 @import url("../style/general.css");
+#nav a.router-link-exact-active {
+  color: #019160;
+}
+.navbar .navbar-content .nav-item.router-link-exact-active {
+  background-color: #019160;
+}
 </style>
